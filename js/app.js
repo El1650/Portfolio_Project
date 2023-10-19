921b7e72cea250c330f5a5bb8ef5ef47cee5b5b6
Elena G.Mandefro <elamandefro@gmail.com>
2023-10-19 14:45:50 +0530

    Updated the home page

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var options = {
+  strings: [
+    "Frontend-Developer",
+    "Freelancer",
+    "Photographer",
+    "Digital Marketer",
+  ],
+  typeSpeed: 100, // typing speed in milliseconds
+  backSpeed: 50,
+  backDelay: 1000, // backspacing speed in milliseconds
+  loop: true, // loop the animation
+};
+var typed = new Typed("#typed-element", options);
